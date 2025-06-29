994d4fa7a67fd8f1a6025059f3fd8b96ce5e9aae
herougo <herougo@gmail.com>
2025-06-29 13:12:00 -0400

    WpfNetFramework: added store, textbox/button, 1st view model

--- WpfNetFramework/WpfNetFramework/App.xaml.cs
+++ WpfNetFramework/WpfNetFramework/App.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfNetFramework.Stores;
+using WpfNetFramework.ViewModels;
 
 namespace WpfNetFramework
 {
@@ -13,5 +18,39 @@ namespace WpfNetFramework
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<CounterStore>();
+                    services.AddSingleton<MainWindowViewModel>();
+
+                    services.AddSingleton(s => new MainWindow()
+                    {
+                        DataContext = s.GetRequiredService<MainWindowViewModel>()
+                    });
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
     }
 }
---
+++ WpfNetFramework/WpfNetFramework/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfNetFramework.Core;
+using WpfNetFramework.Stores;
+
+namespace WpfNetFramework.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        CounterStore _counterStore;
+        public int Counter => _counterStore.Value;
+
+        public MainWindowViewModel(CounterStore counterStore)
+        {
+            _counterStore = counterStore;
+
+            _counterStore.ValueChanged += OnValueChanged;
+        }
+
+        private void OnValueChanged()
+        {
+            OnPropertyChanged(nameof(Counter));
+        }
+    }
+}
