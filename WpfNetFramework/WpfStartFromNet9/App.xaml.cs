ab640a9b295684c594ec93ed9d6b065da28dd23e
herougo <herougo@gmail.com>
2025-07-20 16:07:33 -0400

    WpfStartFromNet9: Added NotifyIcon

--- WpfNetFramework/WpfStartFromNet9/App.xaml.cs
+++ WpfNetFramework/WpfStartFromNet9/App.xaml.cs
@@ -16,12 +16,14 @@ namespace WpfStartFromNet9
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         private readonly IHost _host;
+        private readonly System.Windows.Forms.NotifyIcon _icon;
 
         public App()
         {
+            _icon = new System.Windows.Forms.NotifyIcon();
             _host = Host.CreateDefaultBuilder()
                 .ConfigureServices((hostContext, services) =>
                 {
@@ -40,11 +42,31 @@ namespace WpfStartFromNet9
         {
             _host.Start();
 
+            _icon.Icon = System.Drawing.SystemIcons.WinLogo;
+            _icon.Text = "SingletonSean";
+            _icon.Click += NotifyIcon_Click;
+
+            _icon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            _icon.ContextMenuStrip.Items.Add("Show", new Bitmap(16, 16), OnShowClicked);
+            _icon.Visible = true;
+
             MainWindow = _host.Services.GetRequiredService<MainWindow>();
             MainWindow.Show();
 
             base.OnStartup(e);
         }
+
+        private void OnShowClicked(object? sender, EventArgs e)
+        {
+            System.Windows.MessageBox.Show("Show");
+        }
+
+        private void NotifyIcon_Click(object? sender, EventArgs e)
+        {
+            MainWindow.WindowState = WindowState.Normal;
+            MainWindow.Activate();
+        }
+
 
         protected override void OnExit(ExitEventArgs e)
         {
